19c2a42bc2bb6406596256ece319a4f32443a4ff
Jan Drewniak <jan.drewniak@gmail.com>
2014-07-31 11:45:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,58 @@
-jquery.scrollify
+# jQuery Scrollify Plugin
 ================
 
-A simple jquery plugin to turn a list of images into a scrolling slider with a lightbox. 
+This simple jQuery plugin takes a simple list and turns it into a slider with lightbox functionality, like this: 
+
+![](preview.png)
+ 
+
+## Installation
+
+1. Include jQuery in your html (obviously).  
+2. Download this repo
+3. Stick the `jquery.scrollify.js`, `jquery.scrollify-style.css` files after your jQuery, like so: 
+    ```
+    <link href="*Your CSS Path*/jquery-scrollify-style.css" rel="stylesheet">
+    ```
+    ```
+    <script src="*You JS Path*/jquery.scrollify.js"></script>   
+    ```
+4. Stick font-awesome in your code as well (this is for the nice icons)
+    ```
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.css">
+    ```
+
+## Usage
+
+### JS:
+no options, no arguments, just `.srollify()` an element. 
+```
+$(document).ready(function(){
+    $('.scroller').scrollify(); 
+})
+```
+
+### HTML: 
+There are some stringent markup requirements here. 
+
+The element to scrollify should be a `<ul>`. 
+
+Each `<li>` inside the `<ul>` should contain an `<a>` element. The `<a>` should have an `href` set to the large image, and a `data-title` attribute containing the caption. Inside the `<a>`, there should be a smaller preview image. 
+
+```
+<ul class="scroller"> 
+    <li>
+        <a href="http://placekitten.com/800/800" data-title="caption"> 
+	        <img src="http://placekitten.com/300/300"> 
+        </a>
+    </li>
+</ul> 
+```
+
+## Usage
+
+Demo 
+
+
+#### License
+MIT License - fork, modify and use however you want.
---
+++ jquery.scrollify.js
@@ -0,0 +1,125 @@
+(function ( $ ) {
+	$.fn.scrollify = function() {
+		
+
+		function visuallyWrap(el){ 
+			
+			var ul = $(el)
+			
+			ul.addClass('jq-scroller-mover');
+			
+			ul.wrap('<div class="jq-scroller-wrapper"><div class="jq-scroller"></div></div>');
+			
+			var prevNextBtn = '<i class="fa fa-arrow-circle-left jq-scroller-prev"></i><i class="fa fa-arrow-circle-right jq-scroller-next"></i>'
+			
+			var overlay = '<div class="jq-scroller-overlay"><div class="jq-overlay-item"><i class="jq-overlay-close fa fa-times-circle"></i><i class="fa fa-arrow-circle-left jq-scroller-overlay-prev"></i><i class="fa fa-arrow-circle-right jq-scroller-overlay-next"></i><div class="jq-overlay-content"></div></div></div>';
+			
+			var wrapper = ul.parents('.jq-scroller-wrapper')
+			
+			wrapper.append(prevNextBtn)
+			
+			wrapper.append(overlay)
+
+			$(el).find('li').each(function(index, elem){
+				var li = $(elem)
+				var a = li.find('a').first(); 
+				a.addClass('jq-scroller-preview')
+				li.addClass('jq-scroller-item');
+				a.attr('style', 'background-image:url('+a.attr('href')+');')
+				li.append('<div class="jq-scroller-caption">'+a.attr('data-title')+'</div>')
+			})
+		}
+
+		function attachEventHandlers(el){
+
+			var wrapper = $(el).parents('.jq-scroller-wrapper')
+			
+			var nextBtn = wrapper.find('.jq-scroller-next')
+			
+			var prevBtn = wrapper.find('.jq-scroller-prev')
+			
+			var mover = wrapper.find('.jq-scroller-mover')
+			
+			var overlay = wrapper.find('.jq-scroller-overlay')
+			
+			var items = wrapper.find('.jq-scroller-item')
+			
+			var overlayContent = wrapper.find('.jq-overlay-content')
+			
+			var overlayN = wrapper.find('.jq-scroller-overlay-next')
+			
+			var overlayP = wrapper.find('.jq-scroller-overlay-prev')
+
+			var w = $(items[0]).outerWidth()+"px"; 
+			
+			var animN = function(){
+				var l = $(items).last().offset().left + $(items).last().width(); 
+				var m = $(wrapper).offset().left + $(wrapper).width(); 
+				if ( l >= m){
+					return {left: "-="+w}
+				} else {
+					return {left: "-="+0}	
+				}
+
+			}; 
+			
+			var animP = function(){
+				if ($(mover).position().left == 0){
+					return {left: "+="+0}
+				} else {
+					return {left: "+="+w}	
+				}
+			}; 
+			var animDur = {duration: "medium"}
+
+			var indexOfCurrentItem; 
+
+			$(nextBtn).click( function(){
+				mover.animate(animN(),animDur)
+				console.log($(mover).position().left); 
+			})
+
+			$(prevBtn).click( function(){
+				mover.animate(animP(),animDur)
+				console.log(); 
+			})
+			
+			$(overlay).click( function(){
+				overlay.removeClass('active')
+				overlayContent.empty(); 
+			});
+			
+			$(items).click( function(event){
+				event.preventDefault(); 
+				overlayContent.empty(); 
+				indexOfCurrentItem = items.index(this)
+				var contentLink =  $(this).children('.jq-scroller-preview')
+				var contentImageSrc = contentLink.attr('href')
+				var caption = contentLink.attr('data-title')
+				var content = $('<img src="'+contentImageSrc+'"/><p>'+caption+'</p>');
+				content.appendTo(overlayContent);
+				overlay.addClass('active')
+			})
+
+
+			$(overlayN).click( function(event){
+				event.stopPropagation(); 
+				$(items[indexOfCurrentItem+1]).trigger('click')
+			})
+
+			$(overlayP).click( function(event){
+				event.stopPropagation(); 
+				$(items[indexOfCurrentItem-1]).trigger('click')
+			})
+
+
+		}
+		
+		visuallyWrap(this);
+
+		attachEventHandlers(this)
+
+		return this; 
+	}
+
+}( jQuery ));
